7453470de3dc68aab5bfe390cd92ffb0ac5e7aa9
Dhaulagiri0 <yiqinw4@gmail.com>
2021-02-20 12:22:54 +0800

    adjusted training

--- discriminator.py
+++ discriminator.py
@@ -19,7 +19,7 @@ class Discriminator:
     filters = [512, 512, 512, 512, 256, 128, 64, 32]
     f = filters[cur_block - 1]
     # weight initialization
-    init = RandomNormal(mean=0., stddev=1.)
+    # init = RandomNormal(mean=0., stddev=1.)
     # get shape of existing model
     in_shape = list(old_model.input.shape)
     # define new input shape as double the size
@@ -27,17 +27,17 @@ class Discriminator:
     in_image = Input(shape=input_shape)
     # define new input processing layer
     if cur_block > 3:
-      d = Conv2DEQ(f/2, (1,1), padding='same', kernel_initializer=init, name='d_conv_' + str(cur_block) + '_1')(in_image)
+      d = Conv2DEQ(f/2, (1,1), padding='same', name='d_conv_' + str(cur_block) + '_1')(in_image)
     else:
-      d = Conv2DEQ(f, (1,1), padding='same', kernel_initializer=init, name='d_conv_' + str(cur_block) + '_1')(in_image)
+      d = Conv2DEQ(f, (1,1), padding='same', name='d_conv_' + str(cur_block) + '_1')(in_image)
     d = LeakyReLU(alpha=0.2, name='d_relu_' + str(cur_block) + '_1')(d)
     # define new block
     if cur_block > 3:
-      d = Conv2DEQ(f/2, (3,3), padding='same', kernel_initializer=init, name='d_conv_' + str(cur_block) + '_2')(d)
+      d = Conv2DEQ(f/2, (3,3), padding='same', name='d_conv_' + str(cur_block) + '_2')(d)
     else:
-      d = Conv2DEQ(f, (3,3), padding='same', kernel_initializer=init, name='d_conv_' + str(cur_block) + '_2')(d)
+      d = Conv2DEQ(f, (3,3), padding='same', name='d_conv_' + str(cur_block) + '_2')(d)
     d = LeakyReLU(alpha=0.2, name='d_relu_' + str(cur_block) + '_2')(d)
-    d = Conv2DEQ(f, (3,3), padding='same', kernel_initializer=init, name='d_conv_' + str(cur_block) + '_3')(d)
+    d = Conv2DEQ(f, (3,3), padding='same', name='d_conv_' + str(cur_block) + '_3')(d)
     d = LeakyReLU(alpha=0.2, name='d_relu_' + str(cur_block) + '_3')(d)
     d = AveragePooling2D(name='d_avgpool_' + str(cur_block) + '_1')(d)
     block_new = d
@@ -67,22 +67,22 @@ class Discriminator:
   # define base discriminator
   def define_discriminator(self, input_shape=(4,4,3)):
     # weight initialization
-    init = RandomNormal(mean=0., stddev=1.)
+    # init = RandomNormal(mean=0., stddev=1.)
     # base model input
     in_image = Input(shape=input_shape)
     # conv 1x1
-    d = Conv2DEQ(512, (1,1), padding='same', kernel_initializer=init, name='d_conv_0_1')(in_image)
+    d = Conv2DEQ(512, (1,1), padding='same', name='d_conv_0_1')(in_image)
     d = LeakyReLU(alpha=0.2, name='d_relu_0_1')(d)
     # conv 3x3 (output block)
     d = MinibatchStdev(name='d_ministdev_0_1')(d)
-    d = Conv2DEQ(512, (3,3), padding='same', kernel_initializer=init, name='d_conv_0_2')(d)
+    d = Conv2DEQ(512, (3,3), padding='same', name='d_conv_0_2')(d)
     d = LeakyReLU(alpha=0.2, name='d_relu_0_2')(d)
     # conv 4x4
-    d = Conv2DEQ(512, (4,4), padding='same', kernel_initializer=init, name='d_conv_0_3')(d)
+    d = Conv2DEQ(512, (4,4), padding='valid', name='d_conv_0_3')(d)
     d = LeakyReLU(alpha=0.2, name='d_relu_0_3')(d)
     # dense output layer
     d = Flatten(name='d_flatten_0_1')(d)
-    out_class = DenseEQ(1, activation='linear', name='d_dense_0_1')(d)
+    out_class = DenseEQ(1, name='d_dense_0_1')(d)
     # define model
     model = Model(in_image, out_class)
     self.model = model
--- generator.py
+++ generator.py
@@ -19,25 +19,26 @@ class Generator:
     filters = [512, 512, 512, 256, 128, 64, 32, 16]
     f = filters[cur_block - 1]
     # weight initialization
-    init = RandomNormal(mean=0., stddev=1.)
+    # init = RandomNormal(mean=0., stddev=1.)
     # get the end of the last block
     block_end = old_model.layers[-2].output
     # upsample, and define new block
     upsampling = UpSampling2D(name='g_up2d_' + str(cur_block))(block_end)
-    g = Conv2DEQ(f, (3,3), padding='same', kernel_initializer=init, name='g_conv_' + str(cur_block) + '_1')(upsampling)
+    g = Conv2DEQ(f, (3,3), padding='same', name='g_conv_' + str(cur_block) + '_1')(upsampling)
     g = LeakyReLU(alpha=0.2, name='g_relu_' + str(cur_block) + '_1')(g)
     g = PixelNormalization(name='g_pxnorm_' + str(cur_block) + '_1')(g)
-    g = Conv2DEQ(f, (3,3), padding='same', kernel_initializer=init, name='g_conv_' + str(cur_block) + '_2')(g)
+    g = Conv2DEQ(f, (3,3), padding='same', name='g_conv_' + str(cur_block) + '_2')(g)
     g = LeakyReLU(alpha=0.2, name='g_relu_' + str(cur_block) + '_2')(g)
     g = PixelNormalization(name='g_pxnorm_' + str(cur_block) + '_2')(g)
     # add new output layer
-    out_image = Conv2DEQ(3, (1,1), padding='same', kernel_initializer=init, name='g_conv_' + str(cur_block) + '_3')(g)
+    out_image = Conv2DEQ(3, (1,1), padding='same', name='g_conv_' + str(cur_block) + '_3')(g)
     # define model
     model1 = Model(old_model.input, out_image)
     # get the output layer from old model
     out_old = old_model.layers[-1]
     # connect the upsampling to the old output layer
-    out_image2 = out_old(upsampling)
+    out_image2 = out_old(upsampl
+    ing)
     # define new output image as the weighted sum of the old and new models
     merged = WeightedSum(name='g_wsum_' + str(cur_block) + '_1')([out_image2, out_image])
     # define fade-in model
@@ -51,25 +52,25 @@ class Generator:
   # define base generator
   def define_generator(self, latent_dim, in_dim=4):
     # weight initialization
-    init = RandomNormal(mean=0., stddev=1.)
+    # init = RandomNormal(mean=0., stddev=1.)
     # base model latent input
     in_latent = Input(shape=(latent_dim,))
     # normalise latent features
     g = PixelNormalization(name='g_pxnorm_0_1')(in_latent)
     # linear scale up to activation maps
-    g = DenseEQ(512 * in_dim * in_dim, kernel_initializer=init, name='g_dense_0_1')(g)
+    g = DenseEQ(512 * in_dim * in_dim, name='g_dense_0_1')(g)
     g = Reshape((in_dim, in_dim, 512), name='g_reshape_0_1')(g)
     g = PixelNormalization(name='g_pxnorm_0_2')(g)
     # conv 4x4, input block
-    g = Conv2DEQ(512, (4,4), padding='same', kernel_initializer=init, name='g_conv_0_1')(g)
+    g = Conv2DEQ(512, (4,4), padding='same', name='g_conv_0_1')(g)
     g = LeakyReLU(alpha=0.2, name='g_relu_0_1')(g)
     g = PixelNormalization(name='g_pxnorm_0_3')(g)
     # conv 3x3
-    g = Conv2DEQ(512, (3,3), padding='same', kernel_initializer=init, name='g_conv_0_2')(g)
+    g = Conv2DEQ(512, (3,3), padding='same', name='g_conv_0_2')(g)
     g = LeakyReLU(alpha=0.2, name='g_relu_0_2')(g)
     g = PixelNormalization(name='g_pxnorm_0_4')(g)
     # conv 1x1, output block
-    out_image = Conv2DEQ(3, (1,1), padding='same', kernel_initializer=init, name='g_conv_0_3')(g)
+    out_image = Conv2DEQ(3, (1,1), padding='same', name='g_conv_0_3')(g)
     # define model
     model = Model(in_latent, out_image)
     self.model = model
